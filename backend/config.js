a594b53bedeac71aacfde4ef0fcf7db8ece7e826
Ahmed Wala Zoghlami <zoghlamiahmed1993@gmail.com>
2023-10-17 15:15:20 +0100

    first commit

---
+++ backend/config.js
@@ -0,0 +1,7 @@
+export const PORT = 5555;
+
+
+export const mongoDBURL ="mongodb+srv://root:root@books-store.bnydkpj.mongodb.net/books-collection?retryWrites=true&w=majority"
+
+// Please create a free database for yourself.
+// This database will be deleted after tutorial
